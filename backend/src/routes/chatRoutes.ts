78723e05cdbccee209b63b7199f876fb783df3b5
Mrityunjay1008 <sharmagopal1306@gmail.com>
2026-06-30 14:53:18 +0530

    Api Routes

--- backend/src/routes/chatRoutes.ts
+++ backend/src/routes/chatRoutes.ts
@@ -1,7 +1,12 @@
 import express from 'express'
+import { protectRoute } from '../middleware/auth'
+import { getChats, getOrCreateChat } from '../controllers/chatController'
 
 const router = express.Router()
 
+router.use(protectRoute)
 
+router.get('/',getChats)
+router.get('/with/:participantId',getOrCreateChat)
 
 export default router;
